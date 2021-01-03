980941199b416724489b5700931a637d3aec8d54
Onias da Rocha <onias.filho@hotmail.com>
2021-01-03 12:04:23 -0400

    Created owner index page and controller. Closes #13

---
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controller/OwnerController.java
@@ -0,0 +1,15 @@
+package guru.springframework.sfgpetclinic.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class OwnerController {
+
+    @RequestMapping({"/owners", "/owners/index", "/owners/index.html"})
+    public String getIndex(){
+
+        return "/owners/index";
+    }
+
+}
