46e6c5f678a0461fa87b7fa707ebfde0470bf22f
Onias da Rocha <onias.filho@hotmail.com>
2021-01-03 12:10:18 -0400

    Minor correction on the previous commit

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controller/OwnerController.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controller/OwnerController.java
@@ -3,11 +3,12 @@ package guru.springframework.sfgpetclinic.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@RequestMapping("/owners")
 @Controller
 public class OwnerController {
 
-    @RequestMapping({"/owners", "/owners/index", "/owners/index.html"})
-    public String getIndex(){
+    @RequestMapping({"", "/index", "/index.html"})
+    public String listOwners(){
 
         return "/owners/index";
     }
